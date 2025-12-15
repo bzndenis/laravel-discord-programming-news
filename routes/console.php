5023aac19d66f15d8cc6d5e694632214cffe94f0
Denis Akbar <bzndenis@gmail.com>
2025-12-15 12:21:05 +0700

    feat: add inspire command and schedule security scan with logging

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,11 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Illuminate\Support\Facades\Schedule::command('security:scan')->everyThirtyMinutes();
+Illuminate\Support\Facades\Schedule::command('security:scan')
+    ->everyThirtyMinutes()
+    ->onSuccess(function () {
+        Illuminate\Support\Facades\Log::info('Security scan executed successfully via CRON');
+    })
+    ->onFailure(function () {
+        Illuminate\Support\Facades\Log::error('Security scan FAILED via CRON');
+    });
